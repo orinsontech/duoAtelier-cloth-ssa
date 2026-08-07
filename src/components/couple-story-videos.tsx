91fc066b5bc018a8404b3116061c3c854e74daa3
Vikash KK <hello@vikashkk.com>
2026-08-07 23:28:01 +0530

    Update

--- src/components/couple-story-videos.tsx
+++ src/components/couple-story-videos.tsx
@@ -5,18 +5,15 @@ import { useRef, useState } from 'react';
 const stories = [
   {
     src: 'https://embed-ssl.wistia.com/deliveries/22809aeb63074aa0c6d7db4f5340ee957db61a74.bin',
-    poster:
-      'https://instant-playback.wistia.com/hls/Y2I2ZGUyOTI4NjMwOTIxOS9tZWRpYS0xNTA3OTk2MzQtaW5zdGFudC1obHMtMTI4MHAtMTBhNGI2NzAvdjAwMDAzLXByb2Q=/still-image.jpg',
+    poster: '/assets/couple-story-1-poster.jpg',
   },
   {
     src: 'https://embed-ssl.wistia.com/deliveries/1116e7cc6fc6808b3ba0916f09ed1072620e7309.bin',
-    poster:
-      'https://instant-playback.wistia.com/hls/MTdmMDY5OWI2MGM2NmUwMi9tZWRpYS0xNTA3OTk3MDQtaW5zdGFudC1obHMtMTI4MHAtMzM3ZjA3NWIvdjAwMDAzLXByb2Q=/still-image.jpg',
+    poster: '/assets/couple-story-2-poster.jpg',
   },
   {
     src: 'https://embed-ssl.wistia.com/deliveries/9c01e5fee9e5b11af77fc6b57cc947a8cca4553d.bin',
-    poster:
-      'https://instant-playback.wistia.com/hls/OThkNTRkOTk4YjcxZjlkYy9tZWRpYS0xNTA3OTk5MDYtaW5zdGFudC1obHMtMTI4MHAtZjY1M2MzN2YvdjAwMDAzLXByb2Q=/still-image.jpg',
+    poster: '/assets/couple-story-3-poster.jpg',
   },
 ];
 
